945edff81d363b1bb18d07ff722ecd7ce096b10a
t00n08 <t00n.08.c@gmail.com>
2026-04-01 21:52:12 -0300

    ok

---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+NUM1 = int(input("Ingrese el primer número: "))
+NUM2 = int(input("Ingrese el segundo número: "))
+
+result = NUM1 / NUM2
+print("La división... es:", result)
+
+
+texto = "llanos juan"
+print(texto[7:11])  
