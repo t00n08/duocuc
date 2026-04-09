891a2d15e06609e12f68355b6d590a4ba6f6ab56
t00n08 <t00n.08.c@gmail.com>
2026-04-08 21:30:01 -0400

    ok

---
+++ ejercicio3.py
@@ -0,0 +1,10 @@
+edad = 50
+
+if edad < 18 :
+    print("eres menor de edad")
+elif 18 <= edad <= 40:
+    print("eres adulto joven")
+elif 41 <= edad <= 61:
+    print("eres adulto")
+else:
+    print("eres adulto mayor")
